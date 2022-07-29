d32dc0c2b3936bcd6ef96cdac4d4eec1ed110a26
Ngo Minh Nhut <minhnhut161098@gmail.com>
2022-07-29 11:50:17 +0700

    change

--- apps/products-idiot/pages/index.tsx
+++ apps/products-idiot/pages/index.tsx
@@ -53,20 +53,6 @@ export function Index() {
                     $1999
                 </td>
             </tr>
-            <tr className="bg-white dark:bg-gray-800">
-                <th scope="row" className="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                    Magic Mouse 2
-                </th>
-                <td className="py-4 px-6">
-                    Black
-                </td>
-                <td className="py-4 px-6">
-                    Accessories
-                </td>
-                <td className="py-4 px-6">
-                    $99
-                </td>
-            </tr>
         </tbody>
     </table>
 </div>

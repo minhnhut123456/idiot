69576fc45dffb55e3dc3435328aaa778808bebd9
Ngo Minh Nhut <minhnhut161098@gmail.com>
2022-08-02 17:32:51 +0700

    test change

--- apps/products-idiot/pages/index.tsx
+++ apps/products-idiot/pages/index.tsx
@@ -39,7 +39,7 @@ export function Index() {
                     $2999
                 </td>
             </tr>
-            <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+            {/* <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
                 <th scope="row" className="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
                     Microsoft Surface Pro
                 </th>
@@ -52,7 +52,7 @@ export function Index() {
                 <td className="py-4 px-6">
                     $1999
                 </td>
-            </tr>
+            </tr> */}
         </tbody>
     </table>
 </div>
